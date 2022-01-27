a8f18e86f23348c78c7aa81f94ac5b6094c111cb
Shenandoah Grace <69861032+shenando@users.noreply.github.com>
2022-01-27 10:26:24 -0500

    Update main.js
    update jepBlock styles, add initial media query

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const answerEl = document.getElementById('answerEl')
 const quoteEl = document.getElementById('question')
 const cat = document.getElementById('cat')
+const points = document.getElementById('points')
 const answerDiv = document.getElementById('answerDiv')
 const revealEl = document.getElementById('reveal')
 const btn = document.getElementById('btn')
@@ -20,12 +21,15 @@ async function generateQuote() {
   const res = await fetch('https://jservice.io/api/random', config)
 
   const data = await res.json()
+
+  console.log(data[0])
   
   answerDiv.style.display = 'none'
   revealEl.style.display = 'block'
-  cat.innerHTML = "Category: " + data[0].category.title
+  cat.innerHTML = data[0].category.title.toUpperCase()
   quoteEl.innerHTML = data[0].question
-  answerEl.innerHTML = data[0].answer
+  answerEl.innerHTML = data[0].answer.toUpperCase()
+  points.innerHTML = "$" + data[0].value
 
   // const toSay = data[0]
   // const utterance = new SpeechSynthesisUtterance(toSay)
